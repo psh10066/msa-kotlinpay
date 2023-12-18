bcf35f8ded8f4501ae24048e13c3b0949684eb4e
이호준 <psh10066@gmail.com>
2023-12-18 16:40:51 +0900

    #1 프로젝트 생성
    - Spring Boot 3.2.0
    - Kotlin + JDK 21

---
+++ src/main/kotlin/com/psh10066/kotlinpay/KotlinpayApplication.kt
@@ -0,0 +1,11 @@
+package com.psh10066.kotlinpay
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinpayApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinpayApplication>(*args)
+}
---
+++ src/test/kotlin/com/psh10066/kotlinpay/KotlinpayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.psh10066.kotlinpay
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinpayApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
